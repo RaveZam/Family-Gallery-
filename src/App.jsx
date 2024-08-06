645e096bf086e585f74b9a9471a558e740f45650
Duke <114975455+RaveZam@users.noreply.github.com>
2024-08-06 22:49:19 +0800

    Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Hero from "./hero/Hero";
 import Gallery from "./gallery/Gallery";
 import Header from "./headercomponents/Header";
 function App() {
   return (
     <div>
       <Header />
+      <Hero />
       {/* <Landingpage /> */}
       <Gallery />
     </div>
---
+++ src/hero/Hero.jsx
@@ -0,0 +1,5 @@
+import styles from "./hero.module.css";
+
+export default function Hero() {
+  return <div className={styles.hero}> </div>;
+}
