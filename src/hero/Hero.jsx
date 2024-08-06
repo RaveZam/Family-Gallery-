9fc4e334ec6842b017be3627cd7c84a0c4c24ebe
Duke <114975455+RaveZam@users.noreply.github.com>
2024-08-06 23:29:37 +0800

    Hero3

--- src/hero/Hero.jsx
+++ src/hero/Hero.jsx
@@ -3,6 +3,7 @@ import styles from "./hero.module.css";
 export default function Hero() {
   return (
     <div className={styles.hero}>
+      <h1 className={styles.herotxt}> Life is A Journey </h1>
       <img className={styles.herobg} src="./images/image2.jpg" alt="" />
     </div>
   );
