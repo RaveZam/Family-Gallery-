886d5cdb0fd71beb0f99341191f486736e310c2c
Duke <114975455+RaveZam@users.noreply.github.com>
2024-08-06 22:57:12 +0800

    bg2

--- src/hero/Hero.jsx
+++ src/hero/Hero.jsx
@@ -1,5 +1,9 @@
 import styles from "./hero.module.css";
 
 export default function Hero() {
-  return <div className={styles.hero}> </div>;
+  return (
+    <div className={styles.hero}>
+      <img className={styles.herobg} src="./images/image2.jpg" alt="" />
+    </div>
+  );
 }
