d45850d1082cc31f8331e48d18fbe80cb33091ea
Duke <114975455+RaveZam@users.noreply.github.com>
2024-08-09 21:50:27 +0800

    Added Animations on Gallery

--- src/gallery/Gallery.jsx
+++ src/gallery/Gallery.jsx
@@ -1,5 +1,5 @@
 import LightGallery from "lightgallery/react";
-import { useInView } from "react-hook-inview";
+import { useInView } from "react-intersection-observer";
 import styles from "./gallery.module.css";
 // import styles
 import "lightgallery/css/lightgallery.css";
@@ -16,16 +16,28 @@ export default function Gallery() {
   };
 
   const [ref, isVisible] = useInView({
-    threshold: 1,
+    threshold: 0,
+    rootMargin: "300px",
+    triggerOnce: true,
+  });
+  const [ref2, isVisible2] = useInView({
+    threshold: 0,
+    rootMargin: "550px",
+    triggerOnce: true,
+  });
+  const [ref3, isVisible3] = useInView({
+    threshold: 0,
+    rootMargin: "550px",
+    triggerOnce: true,
+  });
+  const [ref4, isVisible4] = useInView({
+    threshold: 0,
+    rootMargin: "550px",
+    triggerOnce: true,
   });
 
   return (
     <div className={styles.app}>
-      {/* <div className={styles.gdiv}>
-        <h1 className={styles.galleryh1}>
-          <em> We Captured </em> It
-        </h1>
-      </div> */}
       <div className={styles.gallery}>
         <div className={styles.column}>
           <LightGallery
@@ -35,29 +47,34 @@ export default function Gallery() {
           >
             <a ref={ref} href="./images/image2.jpg">
               <img
-                className={styles.image}
+                className={`${styles.image} ${isVisible ? styles.visible : ""}`}
                 alt="img2"
                 src="./images/image2.jpg"
               />
-              {console.log(isVisible ? "I Am Visible!" : "Im Gone bish")}
             </a>
-            <a href="./images/image1.jpg">
+            <a ref={ref2} href="./images/image1.jpg">
               <img
-                className={styles.image}
+                className={`${styles.image} ${
+                  isVisible2 ? styles.visible : ""
+                }`}
                 alt="img1"
                 src="./images/image1.jpg"
               />
             </a>
-            <a href="./images/image7.jpg">
+            <a ref={ref3} href="./images/image7.jpg">
               <img
-                className={styles.image}
+                className={`${styles.image} ${
+                  isVisible3 ? styles.visible : ""
+                }`}
                 alt="img7"
                 src="./images/image7.jpg"
               />
             </a>
-            <a href="./images/image8.jpg">
+            <a ref={ref4} href="./images/image8.jpg">
               <img
-                className={styles.image}
+                className={`${styles.image} ${
+                  isVisible4 ? styles.visible : ""
+                }`}
                 alt="img8"
                 src="./images/image8.jpg"
               />
@@ -72,21 +89,27 @@ export default function Gallery() {
           >
             <a href="./images/image5.jpg">
               <img
-                className={styles.image}
+                className={`${styles.image} ${
+                  isVisible ? styles.visible : ""
+                } ${styles.img5}`}
                 alt="img5"
                 src="./images/image5.jpg"
               />
             </a>
             <a href="./images/image6.jpg">
               <img
-                className={styles.image}
+                className={`${styles.image} ${
+                  isVisible2 ? styles.visible : ""
+                } ${styles.img6}`}
                 alt="img6"
                 src="./images/image6.jpg"
               />
             </a>
             <a href="./images/image3.jpg">
               <img
-                className={styles.image}
+                className={`${styles.image} ${
+                  isVisible3 ? styles.visible : ""
+                } ${styles.img3}`}
                 alt="img3"
                 src="./images/image3.jpg"
               />
@@ -94,7 +117,9 @@ export default function Gallery() {
 
             <a href="./images/image4.jpg">
               <img
-                className={styles.image}
+                className={`${styles.image} ${
+                  isVisible4 ? styles.visible : ""
+                } ${styles.img4}`}
                 alt="img4"
                 src="./images/image4.jpg"
               />
@@ -109,28 +134,36 @@ export default function Gallery() {
           >
             <a href="./images/image8.jpg">
               <img
-                className={styles.image}
+                className={`${styles.image} ${
+                  isVisible ? styles.visible : ""
+                } ${styles.img8}`}
                 alt="img8"
                 src="./images/image8.jpg"
               />
             </a>
             <a href="./images/image9.jpg">
               <img
-                className={styles.image}
+                className={`${styles.image} ${
+                  isVisible2 ? styles.visible : ""
+                } ${styles.img9}`}
                 alt="img9"
                 src="./images/image9.jpg"
               />
             </a>
             <a href="./images/image10.jpg">
               <img
-                className={styles.image}
+                className={`${styles.image} ${
+                  isVisible3 ? styles.visible : ""
+                } ${styles.img10}`}
                 alt="img10"
                 src="./images/image10.jpg"
               />
             </a>
             <a href="./images/image7.jpg">
               <img
-                className={styles.image}
+                className={`${styles.image} ${
+                  isVisible4 ? styles.visible : ""
+                } ${styles.img7}`}
                 alt="img7"
                 src="./images/image7.jpg"
               />
