a0e3b2c1e7ffe380310b6da7c59779171a59d66e
Duke <114975455+RaveZam@users.noreply.github.com>
2024-08-09 22:29:11 +0800

    Removed Lazy Loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, { Suspense, lazy } from "react";
 import "./App.css";
 import Hero from "./hero/Hero";
-// import Gallery from "./gallery/Gallery";
-const Gallery = lazy(() => import("./gallery/Gallery"));
+import Gallery from "./gallery/Gallery";
+// const Gallery = lazy(() => import("./gallery/Gallery"));
 import Header from "./headercomponents/Header";
 
 function App() {
@@ -11,9 +11,7 @@ function App() {
       <Header />
       <Hero />
       {/* <Landingpage /> */}
-      <Suspense>
-        <Gallery />
-      </Suspense>
+      <Gallery />
     </div>
   );
 }
