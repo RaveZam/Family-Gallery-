65ef6580ca55f60a0a5c11d9f1f65d8488476421
Duke <114975455+RaveZam@users.noreply.github.com>
2024-08-09 22:06:02 +0800

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
+import { useState } from "react";
 import "./App.css";
 import Hero from "./hero/Hero";
 import Gallery from "./gallery/Gallery";
 import Header from "./headercomponents/Header";
-const [isHeroLoaded, setIsHeroLoaded] = useState(false);
+
 function App() {
+  const [isHeroLoaded, setIsHeroLoaded] = useState(false);
+
   return (
     <div>
       <Header />
-
+      {console.log(isHeroLoaded)}
       <Hero onLoad={() => setIsHeroLoaded(true)} />
       {/* <Landingpage /> */}
-      {isHeroLoaded && (
-        <>
-          <Gallery />
-        </>
-      )}
+      {isHeroLoaded ? <Gallery /> : ""}
     </div>
   );
 }
