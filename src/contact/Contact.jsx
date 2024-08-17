617a23af8c99d0d9fe6da5c84855cef8bd919b44
Duke <114975455+RaveZam@users.noreply.github.com>
2024-08-17 22:39:28 +0800

    Better Contact Section And Footer

--- src/contact/Contact.jsx
+++ src/contact/Contact.jsx
@@ -7,17 +7,21 @@ export default function Contact() {
           <h1 className={styles.contacttxt}>Contact Us</h1>
           <span className={styles.contactspan}>
             Questions about <em>immigration </em>
-            and <em> photography?</em> You can contact us below with:
+            and <em> photography?</em> Contact us below with:
           </span>
         </div>
         <div className={styles.contactscontainer}>
-          <img className={styles.phone} src="./images/phone.png" alt="" />
+          {/* <img className={styles.phone} src="./images/phone.png" alt="" /> */}
           <div className={styles.instagram}>
             <div className={styles.instabg}>
               <img className={styles.insta} src="./images/insta.png" alt="" />
+              <span className={styles.instaspantitle}>Instagram</span>
               <span className={styles.instaspan}>
                 <em>@jushivan </em>
               </span>
+              <button className={`${styles.btn} ${styles.btn1}`} type="button">
+                Contact
+              </button>
             </div>
           </div>
           <div className={styles.gmail}>
@@ -27,9 +31,13 @@ export default function Contact() {
                 src="./images/gmail.png"
                 alt=""
               />
+              <span className={styles.gmailspantitle}>Gmail</span>
               <span className={styles.gmailspan}>
                 <em>olatsfilm@gmail.com </em>
               </span>
+              <button className={`${styles.btn} ${styles.btn1}`} type="button">
+                Contact
+              </button>
             </div>
           </div>
         </div>
