8a2681b83ffa36356a06b98afea73734dceaab78
Duke <114975455+RaveZam@users.noreply.github.com>
2024-08-19 20:48:36 +0800

    Contact Section finished and Finished the X Axis scroll problem

--- src/contact/Contact.jsx
+++ src/contact/Contact.jsx
@@ -55,3 +55,72 @@ export default function Contact() {
     </div>
   );
 }
+// .contactpage {
+//   height: 50vh;
+//   display: flex;
+//   flex-direction: column;
+// }
+// .column2 {
+//   height: 100%;
+//   width: 100%;
+//   z-index: -1;
+//   /* transform: rotate(-90deg); */
+// }
+
+// .column1 {
+//   width: 100%;
+// }
+// .contacttxt {
+//   font-size: 10vw;
+//   padding-top: 10px;
+//   margin-bottom: 10px;
+// }
+
+// .contacttextcontainer {
+//   margin-bottom: 10px;
+//   padding: 10px;
+//   padding-left: 10px;
+//   padding-right: 10px;
+// }
+
+// .contactspan {
+//   font-size: 5vw;
+// }
+
+// .btn {
+//   margin-top: 10%;
+//   font-size: 4vw;
+// }
+
+// .gmailbtn {
+//   width: 65%;
+//   transform: translateY(-38%);
+// }
+
+// .gmailimg {
+//   max-width: 55%;
+//   margin-bottom: -1%;
+// }
+
+// .insta {
+//   max-width: 60%;
+//   /* margin-bottom: -1%; */
+// }
+
+// .gmailspan {
+//   margin-left: 15px;
+//   transform: translateY(-90%);
+//   font-size: 4vw;
+// }
+
+// .instaspan {
+//   transform: translateY(-155%);
+//   font-size: 5vw;
+// }
+// .gmailspantitle {
+//   display: none;
+// }
+
+// .instaspantitle {
+//   display: none;
+// }
