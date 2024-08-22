5e2126b5e8b733114c73df7332fae0ccab8bc95c
Duke <114975455+RaveZam@users.noreply.github.com>
2024-08-22 22:50:53 +0800

    Animations To Mobile Navigation Bar

--- src/headercomponents/Mobilenav.jsx
+++ src/headercomponents/Mobilenav.jsx
@@ -21,19 +21,22 @@ export default function Mobilenav({ show, setShow }) {
       </div>
       <div className={styles.linkscontainer}>
         <ul className={styles.ul}>
-          <li className={styles.li}>
+          <li className={`${styles.li} ${show ? styles.slide : ""}`}>
             <img className={styles.icons} src="./images/photo.png" alt="" />
             Gallery
           </li>
-          <li className={styles.li}>
+          <li className={`${styles.li} ${show ? styles.slide : ""}`}>
             <img className={styles.icons} src="./images/user.png" alt="" />
             Contact
           </li>
-          <li className={styles.li}>
+          <li className={`${styles.li} ${show ? styles.slide : ""}`}>
             <img className={styles.icons} src="./images/info.png" alt="" />
             About Us
           </li>
-          <li onClick={() => closeBtn()} className={styles.li}>
+          <li
+            onClick={() => closeBtn()}
+            className={`${styles.li} ${show ? styles.slide : ""}`}
+          >
             <img className={styles.icons} src="./images/close.png" alt="" />
             Close
           </li>
