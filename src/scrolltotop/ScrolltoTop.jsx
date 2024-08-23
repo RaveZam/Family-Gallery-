ebb8bd49ba0e76c3146fb111e95fa00a83ae3de8
Duke <114975455+RaveZam@users.noreply.github.com>
2024-08-23 16:45:06 +0800

    Finished!

--- src/scrolltotop/ScrolltoTop.jsx
+++ src/scrolltotop/ScrolltoTop.jsx
@@ -1,10 +1,20 @@
 import styles from "./scroll.module.css";
 
 export default function ScrolltoTop() {
+  function scroll() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // Optional: for smooth scrolling
+    });
+  }
   return (
     <div className={styles.circlecontainer}>
       <a className={styles.circlea}>
-        <img className={styles.circleimg} src="./images/up.png" />
+        <img
+          onClick={() => scroll()}
+          className={styles.circleimg}
+          src="./images/up.png"
+        />
       </a>
     </div>
   );
